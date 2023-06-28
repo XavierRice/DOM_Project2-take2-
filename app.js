c9c2eca5e151ee8b5df4b8ef6c09853e9b6737b0
xavierrice <xavier.rice@gmail.com>
2023-06-28 13:33:07 -0400

    added a new transform feauture i just learned

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const test = console.log
 
-const toolsList = document.querySelector("ul")
 
 const form = document.querySelector("form")
 const selectStock = document.querySelector("select")
@@ -18,7 +17,9 @@ selectStock.after(fighterLabel)
 
 const imageInput = document.createElement("input")
 imageInput.setAttribute("type", "text")
+imageInput.classList.add("image")
 imageInput.setAttribute("placeholder", "add img link")
+imageInput.setAttribute("id", "image" )
 
 const imageLabel = document.createElement("label")
 imageLabel.innerText = "Image:"
@@ -31,31 +32,68 @@ const li = document.getElementsByClassName("li_tools")
 
 for(let i = 0; i < li.length; i++){
     
+    const labelReset = document.createElement("label")
+    labelReset.innerText = "Delete"
+  
     const button = document.createElement("button")
     button.setAttribute("type", "radio")
     button.classList.add("li_reset")
+    labelReset.append(button)
+    li[i].append(labelReset)
 
-
-    li[i].append(button)
+    button.addEventListener("click", (event) =>{
+        const li = event.target.parentNode
+        li.parentNode.remove(li)
+    })
 }    
 
-
-
-
-//insert a delete Event Listener on "button" for assets
-
-
-
 const ninjaTool = document.createElement("li")
+const toolsList = document.querySelector("ul")
+
+
 
 
 form.addEventListener("submit", (event)=>{
    event.preventDefault(); 
 
-   const priceInput = event.target["price"].value
-   const nameInput = event.target["name"].value
-   const stockInput = event.target["stock"].value
-   
+   const priceInput = event.target["price"].value;
+   const nameInput = event.target["name"].value;
+   const stockInput = event.target["stock"].value;
+   const imageSRC = event.target["image"].value;
 
+  const newImage = document.createElement("img")
+  newImage.setAttribute("src", imageSRC)
+  ninjaTool.appendChild(newImage)
+  
+  const newH3 = document.createElement("h3")
+  newH3.innerText = `Name:${nameInput}`
+  ninjaTool.appendChild(newH3)
+
+  const newP = document.createElement("p")
+  newP.innerText = `Price: ${priceInput}`
+  ninjaTool.appendChild(newP)
+   
+  toolsList.appendChild(ninjaTool)
+
+  
 
 })
+
+
+function clearForm(){
+  form.reset()
+}
+
+
+const transform = document.getElementsByClassName("transform")
+
+for(let i=0; i < transform.length; i++){
+  
+  function transformX(){
+    transform[i].style.transform = "translateX(100px)"
+  }
+
+
+transform[i].addEventListener("click", transformX)
+
+}
