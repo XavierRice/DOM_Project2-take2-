b900ebd37755a5f401307778fd928908ef961b07
xavierrice <xavier.rice@gmail.com>
2023-06-29 12:05:14 -0400

    tried-without success to add a pop-up window

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const imageInput = document.createElement("input")
 imageInput.setAttribute("type", "text")
 imageInput.classList.add("image")
 imageInput.setAttribute("placeholder", "add img link")
-imageInput.setAttribute("id", "image" )
+imageInput.setAttribute("id", "image")
 
 const imageLabel = document.createElement("label")
 imageLabel.innerText = "Image:"
@@ -30,22 +30,22 @@ fighterLabel.after(imageLabel)
 
 const li = document.getElementsByClassName("li_tools")
 
-for(let i = 0; i < li.length; i++){
-    
-    const labelReset = document.createElement("label")
-    labelReset.innerText = "Delete"
-  
-    const button = document.createElement("button")
-    button.setAttribute("type", "radio")
-    button.classList.add("li_reset")
-    labelReset.append(button)
-    li[i].append(labelReset)
+for (let i = 0; i < li.length; i++) {
 
-    button.addEventListener("click", (event) =>{
-        const li = event.target.parentNode
-        li.parentNode.remove(li)
-    })
-}    
+  const labelReset = document.createElement("label")
+  labelReset.innerText = "Delete"
+
+  const button = document.createElement("button")
+  button.setAttribute("type", "radio")
+  button.classList.add("li_reset")
+  labelReset.append(button)
+  li[i].append(labelReset)
+
+  button.addEventListener("click", (event) => {
+    const li = event.target.parentNode
+    li.parentNode.remove(li)
+  })
+}
 
 const ninjaTool = document.createElement("li")
 const toolsList = document.querySelector("ul")
@@ -53,18 +53,19 @@ const toolsList = document.querySelector("ul")
 
 
 
-form.addEventListener("submit", (event)=>{
-   event.preventDefault(); 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-   const priceInput = event.target["price"].value;
-   const nameInput = event.target["name"].value;
-   const stockInput = event.target["stock"].value;
-   const imageSRC = event.target["image"].value;
+  const priceInput = event.target["price"].value;
+  const nameInput = event.target["name"].value;
+  const stockInput = event.target["stock"].value;
+  const imageSRC = event.target["image"].value;
+
 
   const newImage = document.createElement("img")
   newImage.setAttribute("src", imageSRC)
   ninjaTool.appendChild(newImage)
-  
+
   const newH3 = document.createElement("h3")
   newH3.innerText = `Name:${nameInput}`
   ninjaTool.appendChild(newH3)
@@ -72,28 +73,67 @@ form.addEventListener("submit", (event)=>{
   const newP = document.createElement("p")
   newP.innerText = `Price: ${priceInput}`
   ninjaTool.appendChild(newP)
-   
+
   toolsList.appendChild(ninjaTool)
 
-  
+  setTimeout(clearForm, 3000)
 
 })
 
 
-function clearForm(){
+function clearForm() {
   form.reset()
 }
 
 
 const transform = document.getElementsByClassName("transform")
 
-for(let i=0; i < transform.length; i++){
-  
-  function transformX(){
+for (let i = 0; i < transform.length; i++) {
+
+  function transformX() {
     transform[i].style.transform = "translateX(100px)"
   }
 
 
-transform[i].addEventListener("click", transformX)
+  transform[i].addEventListener("mouseover", transformX)
 
+}
+
+
+const stockButton = document.getElementsByClassName("current")
+const h4Stock = document.getElementsByClassName("stock_status")
+
+
+for (let i = 0; i < stockButton.length; i++) {
+
+  stockButton[i].addEventListener("click", (event) => {
+    h4Stock[i].innerText = "In Stock"
+  })
+
+}
+
+for (let i = 0; i < h4Stock.length; i++) {
+h4Stock[i].addEventListener("click", (event)=>{
+  h4Stock[i].innerText = " Not In Stock"
+})
+
+}
+
+
+const popup = document.getElementById("popup")
+const closeButton= document.getElementsByClassName("close")
+
+for(let i =0; i < transform.length; i++){
+ 
+  transform[i].addEventListener("click", (event) =>{
+    popup.style.display= "block"
+  })
+  
+}
+
+
+for (let i = 0; i < closeButton.length; i++){
+  closeButton[i].addEventListener("click", (event) => {
+    popup.style.display= "none"
+  })
 }
